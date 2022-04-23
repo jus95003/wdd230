f27cf8405a90c5ec91d8143405d71c7dd61b722b
jus95003 <jus95003@byui.edu>
2022-04-22 21:42:55 -0700

    js changes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ let d = new Date();
 let year = d.getFullYear();
 
 document.getElementById("currentyear").textContent = year;
+
+let lastMod = new Date(document.lastModified);
+
+document.getElementById("last-modified").textContent = lastMod;
