4e05e0d5c6f6b7d5bf4aebb2a220d7442ac25ce0
jus95003 <jus95003@byui.edu>
2023-09-28 21:30:03 -0700

    Updates to CSS

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,7 @@
 // Store the selected elements that we are going to use.
 
-const mainnav = document.querySelector('.nav-list')
 const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('.nav-list')
 
 // Add a click event listener to the hamburger button and use a callback function that toggles the list element's list of classes.
 
