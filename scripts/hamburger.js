89bc435204a0f37b5d05e6966ab35bb9333fe677
jus95003 <jus95003@byui.edu>
2023-09-28 21:44:36 -0700

    Updates

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,11 +1,11 @@
 // Store the selected elements that we are going to use.
 
 const hambutton = document.querySelector('#menu');
-const mainnav = document.querySelector('.nav-list')
+const navigation = document.querySelector('.nav-list');
 
 // Add a click event listener to the hamburger button and use a callback function that toggles the list element's list of classes.
 
 hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
+	navigation.classList.toggle('open');
+	hambutton.classList.toggle('open');
 });
