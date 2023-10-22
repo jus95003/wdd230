04d860f42522752cb4990937cf0ee33429f0a706
jus95003 <jus95003@byui.edu>
2023-10-22 15:30:12 -0700

    updated javascript

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function ()
 
     function calculateWindchill()
     {
-        if (temp <= 50 && wind > 3)
+        if (temp <= 50 && wind > 3.0)
         {
             let chill = 35.74 + (0.6215 * temp) - (35.75 * (wind^0.16)) + (0.4275 * temp * (wind^0.16));
 
